b0aaea5e40d5a4c8c7ed551e83891e14c2cd5cdf
paveltrufi <paveltrufi@hotmail.com>
2017-05-06 14:05:44 +0200

    updated with news

--- main.py
+++ main.py
@@ -6,14 +6,21 @@ from datetime import datetime
 app = Flask(__name__)
 
 def receiveNews():
-    er = EventRegistry(apiKey = "7f142189-e6b0-43b0-b66e-f2850b866caf")
-    q = QueryArticles(conceptUri = er.getConceptUri("Violencia de género"))
-    q.addRequestedResult(RequestArticlesInfo(returnInfo = ReturnInfo(articleInfo = ArticleInfoFlags(bodyLen = -1, image = True))))
-    return er.execQuery(q)
+  er = EventRegistry(apiKey = "7f142189-e6b0-43b0-b66e-f2850b866caf")
+  q = QueryArticles(conceptUri = er.getConceptUri("Violencia de género"))
+  q.addRequestedResult(RequestArticlesInfo(returnInfo = ReturnInfo(articleInfo = ArticleInfoFlags(bodyLen = -1, image = True))))
+  return er.execQuery(q)
 
+last_date = datetime.now()
+res = receiveNews()
 @app.route('/')
 def hello_world():
-  return jsonify(receiveNews())    
+  global last_date, res
+  current_date = datetime.now()
+  if (current_date - last_date).total_seconds() > 30:
+    last_date = current_date
+    res = receiveNews()
+  return jsonify(res)
 
 
 if __name__ == '__main__':
