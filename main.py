9cf546d944cefebfe5a399e5aec3e64dd90e8e4b
paveltrufi <paveltrufi@hotmail.com>
2017-05-06 13:48:02 +0200

    updated with news

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+# -*- coding: utf-8 -*-
 from flask import Flask
+from eventregistry import *
+from datetime import datetime
 app = Flask(__name__)
+
+def receiveNews():
+    er = EventRegistry(apiKey = "7f142189-e6b0-43b0-b66e-f2850b866caf")
+    q = QueryArticles(conceptUri = er.getConceptUri("Violencia de género"))
+    q.addRequestedResult(RequestArticlesInfo(returnInfo = ReturnInfo(articleInfo = ArticleInfoFlags(bodyLen = -1, image = True))))
+    return er.execQuery(q)
 
 @app.route('/')
 def hello_world():
   return 'Hello, Azure!'
+
+@app.route("/get-news")
+def getNews():
+    return jsonify(receiveNews())
+    
+
 
 if __name__ == '__main__':
   app.run()
